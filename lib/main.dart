ddb50606e1a3607fcce14e8ff2df5cac08e4ba1f
hashem sheikhypour <sh.sheikhypour@gmail.com>
2022-01-04 19:53:01 +0330

    make Scaffold and test debug pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,24 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text('hello world !'),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
