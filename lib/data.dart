e07e03a5d33e5868a76573bd70feb8b555e36305
hashem sheikhypour <sh.sheikhypour@gmail.com>
2022-01-06 11:33:38 +0330

    change Assets and font of Project

--- lib/data.dart
+++ lib/data.dart
@@ -1,11 +1,11 @@
-class Story {
+class StoryData {
   final int id;
   final String name;
   final String imageFileName;
   final String iconFileName;
   final bool isViewed;
 
-  Story(
+  StoryData(
       {required this.id,
       required this.name,
       required this.imageFileName,
@@ -22,55 +22,74 @@ class Category {
       {required this.id, required this.title, required this.imageFileName});
 }
 
+class PostData {
+  final int id;
+  final String caption;
+  final String title;
+  final String likes;
+  final String time;
+  final bool isBookmarked;
+  final String imageFileName;
+
+  PostData(
+      {required this.id,
+      required this.caption,
+      required this.title,
+      required this.likes,
+      required this.time,
+      required this.isBookmarked,
+      required this.imageFileName});
+}
+
 class AppDatabase {
-  static List<Story> get stories {
+  static List<StoryData> get stories {
     return [
-      Story(
+      StoryData(
           id: 1001,
           name: 'Emilia',
-          imageFileName: 'story_1.jpg',
+          imageFileName: 'story_9.jpg',
           iconFileName: 'category_1.png',
           isViewed: false),
-      Story(
+      StoryData(
           id: 1002,
           name: 'Richard',
-          imageFileName: 'story_2.jpg',
+          imageFileName: 'story_10.jpg',
           iconFileName: 'category_2.png',
           isViewed: false),
-      Story(
+      StoryData(
           id: 1003,
           name: 'Jasmine',
-          imageFileName: 'story_3.jpg',
+          imageFileName: 'story_1.jpg',
           iconFileName: 'category_3.png',
           isViewed: true),
-      Story(
+      StoryData(
           id: 1004,
           name: 'Lucas',
-          imageFileName: 'story_4.jpg',
+          imageFileName: 'story_3.jpg',
           iconFileName: 'category_4.png',
           isViewed: false),
-      Story(
+      StoryData(
           id: 1005,
-          name: 'Hendri',
-          imageFileName: 'story_5.jpg',
+          name: 'Isabella',
+          imageFileName: 'story_4.jpg',
           iconFileName: 'category_2.png',
           isViewed: false),
-      Story(
+      StoryData(
           id: 1006,
-          name: 'Hendri',
+          name: 'Olivia',
           imageFileName: 'story_6.jpg',
           iconFileName: 'category_1.png',
           isViewed: false),
-      Story(
+      StoryData(
           id: 1007,
-          name: 'Hendri',
+          name: 'Amelia',
           imageFileName: 'story_7.jpg',
           iconFileName: 'category_4.png',
           isViewed: false),
-      Story(
+      StoryData(
           id: 1008,
-          name: 'Hendri',
-          imageFileName: 'story_8.jpg',
+          name: 'Grace',
+          imageFileName: 'story_2.jpg',
           iconFileName: 'category_3.png',
           isViewed: false),
     ];
@@ -83,14 +102,44 @@ class AppDatabase {
         title: 'Technology',
         imageFileName: 'large_post_1.jpg',
       ),
-      Category(id: 102, title: 'Adventure', imageFileName: 'large_post_2.jpg'),
-      Category(id: 103, title: 'Sports', imageFileName: 'large_post_3.jpg'),
-      Category(id: 104, title: 'Politics', imageFileName: 'large_post_4.jpg'),
-      Category(id: 105, title: 'Science', imageFileName: 'large_post_5.jpg'),
+      Category(id: 102, title: 'Cinema', imageFileName: 'large_post_2.jpg'),
       Category(
-          id: 106,
-          title: 'Car and Motorcycles',
-          imageFileName: 'large_post_6.jpg'),
+          id: 103, title: 'Transportation', imageFileName: 'large_post_3.jpg'),
+      Category(id: 104, title: 'Adventure', imageFileName: 'large_post_4.jpg'),
+      Category(
+          id: 105,
+          title: 'Artificial Intelligence',
+          imageFileName: 'large_post_5.jpg'),
+      Category(id: 106, title: 'Economy', imageFileName: 'large_post_6.jpg'),
+    ];
+  }
+
+  static List<PostData> get posts {
+    return [
+      PostData(
+          id: 1,
+          title: 'BMW M5 Competition Review 2021',
+          caption: 'TOP GEAR',
+          isBookmarked: false,
+          likes: '3.1k',
+          time: '1hr ago',
+          imageFileName: 'small_post_1.jpg'),
+      PostData(
+          id: 0,
+          title: 'MacBook Pro with M1 Pro and M1 Max review',
+          caption: 'THE VERGE',
+          isBookmarked: false,
+          likes: '1.2k',
+          time: '2hr ago',
+          imageFileName: 'small_post_2.jpg'),
+      PostData(
+          id: 2,
+          title: 'Step design sprint for UX beginner',
+          caption: 'Ux Design',
+          isBookmarked: true,
+          likes: '2k',
+          time: '41hr ago',
+          imageFileName: 'small_post_3.jpg'),
     ];
   }
 }
