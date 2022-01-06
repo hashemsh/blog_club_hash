569ba87bfd51577fff43c195f0534268135a38bd
hashem sheikhypour <sh.sheikhypour@gmail.com>
2022-01-06 11:07:27 +0330

    header and Story ListView make

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: unused_import
-
 import 'package:blog_club_hash/data.dart';
 import 'package:flutter/material.dart';
 
@@ -8,15 +6,35 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  static const defaultFontFamily = 'Avenir';
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const primaryTextColor = Color(0xff0D253C);
+    const secondaryTextColor = Color(0xff2D4379);
     return MaterialApp(
       title: 'Blog Club',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: const TextTheme(
+          subtitle1: TextStyle(
+            fontFamily: defaultFontFamily,
+            color: secondaryTextColor,
+            fontSize: 14,
+          ),
+          headline6: TextStyle(
+            fontFamily: defaultFontFamily,
+            fontWeight: FontWeight.bold,
+            color: primaryTextColor,
+          ),
+          bodyText2: TextStyle(
+            fontFamily: defaultFontFamily,
+            color: secondaryTextColor,
+            fontSize: 12,
+          ),
+        ),
       ),
       home: const HomeScreen(),
     );
@@ -28,12 +46,102 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+    final stories = AppDatabase.stories;
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
-            children: const [
-              Text('hello world test !'),
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(32, 16, 32, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Hi, Jonathan !',
+                      style: themeData.textTheme.subtitle1,
+                    ),
+                    Image.asset(
+                      'assets/img/icons/notification.png',
+                      width: 24,
+                      height: 24,
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(32, 0, 32, 24),
+                child: Text(
+                  'Explore today’s',
+                  style: themeData.textTheme.headline6,
+                ),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 90,
+                child: ListView.builder(
+                    itemCount: stories.length,
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.fromLTRB(32, 0, 32, 0),
+                    itemBuilder: (context, index) {
+                      final story = stories[index];
+                      return Container(
+                        margin: const EdgeInsets.fromLTRB(4, 0, 4, 0),
+                        child: Column(
+                          children: [
+                            Stack(
+                              children: [
+                                Container(
+                                  width: 68,
+                                  height: 68,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(24),
+                                    gradient: const LinearGradient(
+                                      begin: Alignment.topLeft,
+                                      colors: [
+                                        Color(0xff376AED),
+                                        Color(0xff49B0E2),
+                                        Color(0xff9CECFB),
+                                      ],
+                                    ),
+                                  ),
+                                  child: Container(
+                                    margin: const EdgeInsets.all(3),
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                    padding: const EdgeInsets.all(4),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(16),
+                                      child: Image.asset(
+                                        'assets/img/stories/${story.imageFileName}',
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Positioned(
+                                  bottom: 0,
+                                  right: 0,
+                                  child: Image.asset(
+                                    'assets/img/icons/${story.iconFileName}',
+                                    width: 24,
+                                    height: 24,
+                                  ),
+                                )
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 8,
+                            ),
+                            Text(story.name),
+                          ],
+                        ),
+                      );
+                    }),
+              )
             ],
           ),
         ),
